8b74cbb9a05622f009e6b0a528755730942e7dfe
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-09 17:18:43 +0100

    add error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
+import Page from "./components/Page";
+import Error from "./components/Error";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Sidebar />
+      <Page />
       <Footer />
+      <Error />
+      <About />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "../components/Navbar.css";
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <p>Navbar</p>
+    </nav>
   );
 }
