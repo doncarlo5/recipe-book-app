3a9d271bff9c53ceafcbb80081d8425de8cc120a
Maria Frih <maria.frih@gmail.com>
2024-01-11 09:16:52 +0100

    sortByCalorie implemented but not working... project needs to be re-organised

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,14 +2,36 @@ import React, { useState } from "react";
 import "./RecipeList.css";
 import JSONrecipes from "../../assets/recipes.json";
 import OneRecipe from "../OneRecipe/OneRecipe";
+import Sidebar from "../Sidebar/Sidebar";
 
 function RecipeList() {
   const [recipes, SetRecipes] = useState(JSONrecipes);
+
+  function handleDelete(id) {
+    const remainingRecipes = recipes.filter((recipe) => {
+      return recipe.id !== id;
+    });
+    SetRecipes(remainingRecipes);
+  }
+
+  function sortByCalories() {
+    const copy = structuredClone(recipes);
+    copy.sort((recipeA, recipeB) => {
+      return recipeA.calories - recipeB.calories;
+    });
+    SetRecipes(copy);
+  }
+
   return (
     <div className="RecipeList">
       {recipes.map((recipe) => (
-        <OneRecipe key={recipe.id} recipe={recipe} />
+        <OneRecipe
+          key={recipe.id}
+          recipe={recipe}
+          handleDelete={handleDelete}
+        />
       ))}
+      <Sidebar sortByCalories={sortByCalories} />
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Sidebar.css";
 
-export default function Sidebar() {
+export default function Sidebar({ sortByCalories }) {
   return (
     <div className="Sidebar">
-      <a href="../About/About.jsx">About</a>
+      <h3>Filters</h3>
+      <button
+        onClick={() => {
+          sortByCalories;
+        }}
+      >
+        Filter by Calories
+      </button>
+      <p>
+        Made with 💚 in Paris. Know more{" "}
+        <a href="../About/About.jsx">about us 👨🏻‍💻👩🏽‍💻</a>
+      </p>
       <a href="../Error/Error.jsx">Error 404</a>
     </div>
   );
