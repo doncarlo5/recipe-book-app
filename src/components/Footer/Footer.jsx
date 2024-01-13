5a6ec9b85b3b08e47842602db4fbf1c7ef42f23d
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-13 19:26:23 +0100

    change sidebar, navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="Footer">
+      <p>
+        Link to{" "}
+        <a href="https://github.com/doncarlo5/recipe-book-app">GitHub</a>. Open
+        to suggestions 🍻
+      </p>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
 import "./Navbar.css";
 
 export default function Navbar() {
   // const [search, setSearch] = useState("");
   return (
-    <nav>
-      <h1>🍱 The Ultimate Recipe Book</h1>
+    <nav className="NavBar">
+      <NavLink to="/">
+        <h1>📖 The Ultimate Recipe Book</h1>
+      </NavLink>
       {/* don't forget to deal with handleSubmit */}
       {/* <form onSubmit={handleSubmit}> */}
-      <input
-        type="search"
-        // value={search}
-        // onChange={(e) => setSearch(e.target.value)}
-        // name=""
-        // id=""
-      />
-      <button>🔍</button>
-      {/* </form> */}
+      <div className="SearchBar">
+        <input
+          type="search"
+          // value={search}
+          // onChange={(e) => setSearch(e.target.value)}
+          // name=""
+          // id=""
+        />
+        <button>🔍</button>
+        {/* </form> */}
+      </div>
     </nav>
   );
 }
