4651a651dad97b16c48b68e7b883376ff0a541db
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-14 22:47:21 +0100

    change styling to sidebar with classname=sidebar-info

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,27 +5,32 @@ import { Link } from "react-router-dom";
 export default function Sidebar({ sortByCalories, toggleVegetarianFilter }) {
   return (
     <div className="Sidebar">
-      <h3>Filters</h3>
-      <div>
-        <input
-          type="checkbox"
-          id="sortbycalories"
-          onChange={(e) => sortByCalories(e.target.checked)}
-        />
-        <label htmlFor="sortbycalories">Sort by Calories</label>
+      <h3>FILTERS</h3>
+      <hr />
+      <div className="Sidebar-checkbox">
+        <div>
+          <input
+            type="checkbox"
+            id="sortbycalories"
+            onChange={(e) => sortByCalories(e.target.checked)}
+          />
+          <label htmlFor="sortbycalories">Sort by Calories ↯</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="vegetarian"
+            onChange={(e) => toggleVegetarianFilter(e.target.checked)}
+          />
+          <label htmlFor="vegetarian">Only vegetarian 🥬</label>
+        </div>
       </div>
-      <div>
-        <input
-          type="checkbox"
-          id="vegetarian"
-          onChange={(e) => toggleVegetarianFilter(e.target.checked)}
-        />
-        <label htmlFor="vegetarian">Only vegetarian recipes</label>
+      <div className="Sidebar-info">
+        <hr />
+        <p>Made with 💚 in Paris.</p>
+        <Link to="/about">ABOUT US 👨🏻‍💻👩🏽‍💻</Link>
+        <Link to="*">ERROR 404 🦴</Link>
       </div>
-      <p>
-        Made with 💚 in Paris. Know more <Link to="/about">about us 👨🏻‍💻👩🏽‍💻</Link>
-      </p>
-      <Link to="*">Error 404</Link>
     </div>
   );
 }
