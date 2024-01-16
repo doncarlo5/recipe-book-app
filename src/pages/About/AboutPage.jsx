7fe745a576c00f5f46d3c5a2ebd15b9e6bd4ace7
Maria Frih <maria.frih@gmail.com>
2024-01-16 20:18:40 +0100

    about page done : content and styling

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,28 +1,51 @@
 import React from "react";
 import "./AboutPage.css";
+import julienPic from "../../assets/images/Julien_Thomas_Ironhack.jpg";
+import mariaPic from "../../assets/images/Maria_FRIH_Ironhack.jpg";
+import linkedinIcon from "../../assets/images/iconmonstr-linkedin-3-240.png";
+import githubIcon from "../../assets/images/iconmonstr-github-3-240.png";
 
 export default function About() {
   return (
     <div>
-      <h1>Hey, we are Julien and Maria 🤌</h1>
+      <h1 className="Bio-presentation">Hey, we are Julien and Maria 🤌</h1>
       <h2>Full-stack developers based in Paris</h2>
       <section className="Bio-section">
         <div className="Individual-bio-card">
-          <div className="img-wrapper">
-            <img
-              src="../../assets/images/Julien_Thomas_Ironhack.jpg"
-              alt="Julien Thomas Ironhack pic"
-            />
+          <div className="Bio-card-content">
+            <img src={julienPic} alt="Julien Thomas Ironhack pic" />
+            <h3 className="Bio-name">Julien Thomas</h3>
+            <p className="Bio-description">
+              🧑🏻‍💻 Hello! I'm Julien THOMAS, a React FullStack Developer from 🇫🇷
+            </p>
           </div>
-          <h3>Julien Thomas</h3>
-          <p>
-            🧑🏻‍💻 Hello! I'm Julien THOMAS, a React FullStack Developer from 🇫🇷.
-            Over the past few months, I've been self-training in programming,
-            and I've recently taken my skills to the next level by joining
-            IronHack School to further my expertise.
-          </p>
+          <div className="Bio-profiles">
+            <a href="https://www.linkedin.com/in/julienthomaspro/">
+              <img src={linkedinIcon} alt="linkedIn Icon" />
+            </a>
+            <a href="https://github.com/doncarlo5/">
+              <img src={githubIcon} alt="Github Icon" />
+            </a>
+          </div>
         </div>
-        <div className="Individual-bio-card"></div>
+        <div className="Individual-bio-card">
+          <div className="Bio-card-content">
+            <img src={mariaPic} alt="Maria FRIH Ironhack pic" />
+            <h3 className="Bio-name">Maria FRIH</h3>
+            <p className="Bio-description">
+              🧑🏻‍💻 Hello! I'm Maria FRIH, a React FullStack Developer from 🇫🇷 and
+              🇲🇦
+            </p>
+          </div>
+          <div className="Bio-profiles">
+            <a href="https://www.linkedin.com/in/maria-frih-a7aa63a/">
+              <img src={linkedinIcon} alt="linkedIn Icon" />
+            </a>
+            <a href="https://github.com/mfrih">
+              <img src={githubIcon} alt="Github Icon" />
+            </a>
+          </div>
+        </div>
       </section>
     </div>
   );
