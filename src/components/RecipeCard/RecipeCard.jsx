a076c28129074c510394b250b77180785f080c41
doncarlos5 <pro.julien.thomas@gmail.com>
2024-01-17 11:17:14 +0100

    add svg

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 import "./RecipeCard.css";
 
+const vegetarianSVG = (
+  <svg
+    width="30px"
+    height="30px"
+    viewBox="0 0 24 24"
+    stroke-width="1.2"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    color="#2ed600"
+  >
+    <path
+      d="M7 21C7 21 7.5 16.5 11 12.5"
+      stroke="#2ed600"
+      stroke-width="1.2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+    <path
+      d="M19.1297 4.24224L19.7243 10.4167C20.0984 14.3026 17.1849 17.7626 13.2989 18.1367C9.486 18.5039 6.03191 15.7168 5.66477 11.9039C5.29763 8.09099 8.09098 4.70237 11.9039 4.33523L18.475 3.70251C18.8048 3.67074 19.098 3.91239 19.1297 4.24224Z"
+      stroke="#2ed600"
+      stroke-width="1.2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+);
+
 function RecipeCard({ recipe, handleDelete }) {
   return (
     <article className="RecipeCard">
@@ -17,6 +44,13 @@ function RecipeCard({ recipe, handleDelete }) {
           <span>Servings:</span>
           {recipe.servings}
         </p>
+        <div className="svg">
+          {recipe.isVegetarian && (
+            <p>
+              <span>{vegetarianSVG}</span>
+            </p>
+          )}
+        </div>
       </div>
       <button onClick={() => handleDelete(recipe.id)}>x</button>
     </article>
